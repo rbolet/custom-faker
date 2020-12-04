b8c72470e15caaf94cfb0a60226af231a82e7dc3
rbolet <rapha.bolet@gmail.com>
2020-12-04 12:10:22 -0800

    init

---
+++ data/realCaAddresses.js
@@ -0,0 +1,201 @@
+[
+  `7928 Summit StreetSan Marcos, CA 92069`,
+  `49 Columbia St.
+Chino Hills, CA 91709`,
+  `9 Sierra St.
+Van Nuys, CA 91406`,
+  `5 Ridgeview Street
+Long Beach, CA 90805`,
+  `735 Wellington St.
+North Hills, CA 91343`,
+  `9843A Depot Court
+El Cajon, CA 92020`,
+  `32 Baker St.
+Riverside, CA 92509`,
+  `139 Poplar Lane
+Vacaville, CA 95687`,
+  `74 Plumb Branch Drive
+Vista, CA 92083`,
+  `8315 Sherman Ave.
+Los Angeles, CA 90003`,
+  `39 Leeton Ridge St.
+Colton, CA 92324`,
+  `805 Oakwood Ave.
+Pico Rivera, CA 90660`,
+  `9176 Ramblewood Avenue
+Fairfield, CA 94533`,
+  `48 NE. High Drive
+Chula Vista, CA 91911`,
+  `70 West Ave.
+Costa Mesa, CA 92627`,
+  `17 Arrowhead Street
+Bakersfield, CA 93306`,
+  `7066 Overlook Drive
+Bakersfield, CA 93307`,
+  `792 East Pennsylvania Court
+Laguna Niguel, CA 92677`,
+  `7 N. Front Street
+Compton, CA 90221`,
+  `28 Ryan St.
+Chino, CA 91710`,
+  `7603 Ramblewood St.
+San Jose, CA 95122`,
+  `7334 South Griffin St.
+Anaheim, CA 92804`,
+  `511C Hill Dr.
+Van Nuys, CA 91405`,
+  `"819 Sunset Dr.
+Reseda, CA 91335`,
+  `74 Oklahoma Street
+North Hollywood, CA 91605`,
+  `30 North Sussex St.
+Sunnyvale, CA 94086`,
+  `871 Ann Rd.
+Los Angeles, CA 90001`,
+  `61 NW. State Drive
+La Habra, CA 90631`,
+  `8598 Manhattan Court
+Oceanside, CA 92056`,
+  `585 East Wood Road
+Corona, CA 92882`,
+  `8674 W. River St.
+San Jose, CA 95116`,
+  `47 Mammoth Dr.
+San Diego, CA 92114`,
+  `28 North Bowman Dr.
+Victorville, CA 92392`,
+  `74 W. Kirkland Ave.
+Fresno, CA 93727`,
+  `7 Penn Dr.
+Fontana, CA 92336`,
+  `621 Durham Ave.
+San Pablo, CA 94806`,
+  `8756 St Margarets Drive
+Anaheim, CA 92801`,
+  `830 Berkshire Street
+Pacoima, CA 91331`,
+  `386 Hamilton Dr.
+Oxnard, CA 93030`,
+  `70 South Van Dyke St.
+National City, CA 91950`,
+  `996 South Canterbury St.
+Long Beach, CA 90813`,
+  `4 Cottage St.
+South San Francisco, CA 94080`,
+  `8126 Fifth Ave.
+Hayward, CA 94541`,
+  `35 New Dr.
+San Francisco, CA 94122`,
+  `112 Henry Smith Ave.
+Porterville, CA 93257`,
+  `8339 South Canal Ave.
+Azusa, CA 91702`,
+  `7920 Miles St.
+San Diego, CA 92117`,
+  `73 Argyle Drive
+Bakersfield, CA 93309`,
+  `98 Forest St.
+Simi Valley, CA 93065`,
+  `47 Glenholme Street
+San Diego, CA 92115`,
+  `91 West 2nd Ave.
+Hayward, CA 94544`,
+  `8756 SE. Smith Store Ave.
+Hesperia, CA 92345`,
+  `217 Hawthorne Rd.
+Anaheim, CA 92805`,
+  `379 Young Ave.
+Pittsburg, CA 94565`,
+  `8000 Sunset Ave.
+Sacramento, CA 95828`,
+  `18 Myers Ave.
+Chula Vista, CA 91910`,
+  `93 Adams St.
+South Gate, CA 90280`,
+  `703 Walnutwood Street
+Tustin, CA 92780`,
+  `88 Livingston Street
+Santa Ana, CA 92701`,
+  `48 Overlook St.
+San Jose, CA 95127`,
+  `493 Cleveland Drive
+Rancho Cucamonga, CA 91730`,
+  `8642 Cedar Ave.
+Santa Ana, CA 92703`,
+  `8 W. Sage Court
+Folsom, CA 95630`,
+  `70 Hudson Ave.
+Hawthorne, CA 90250`,
+  `97 Ocean Drive
+Lancaster, CA 93535`,
+  `8489 Primrose Dr.
+San Bernardino, CA 92404`,
+  `9801 Fairground Dr.
+Moreno Valley, CA 92553`,
+  `6 N. Border Lane
+Oakland, CA 94601`,
+  `89 West Hillside Dr.
+Fountain Valley, CA 92708`,
+  `29 Myrtle St.
+Wilmington, CA 90744`,
+  `18 Country Street
+Los Angeles, CA 90026`,
+  `623 S. Oak Rd.
+Riverside, CA 92503`,
+  `67 SW. Laurel St.
+Panorama City, CA 91402`,
+  `9107 Randall Mill Circle
+Carson, CA 90745`,
+  `7280 West Oxford Court
+Sunnyvale, CA 94087`,
+  `44 Bank St.
+Milpitas, CA 95035`,
+  `9534 North Manhattan Road
+Los Angeles, CA 90006`,
+  `9468 Birch Hill St.
+Los Angeles, CA 90037`,
+  `88 Dogwood Ave.
+Westminster, CA 92683`,
+  `8508 Highland Court
+Sylmar, CA 91342`,
+  `7750 Oakland Court
+Rialto, CA 92376`,
+  `201 Smith St.
+Indio, CA 92201`,
+  `182 Marlborough Street
+Ontario, CA 91762`,
+  `52 King Drive
+Hanford, CA 93230`,
+  `744 Winding Way Avenue
+Tulare, CA 93274`,
+  `485 Broad Street
+San Jose, CA 95112`,
+  `655 William Dr.
+Los Angeles, CA 90022`,
+  `447 North Wood St.
+Union City, CA 94587`,
+  `857 Bowman St.
+San Francisco, CA 94109`,
+  `918 Sussex Ave.
+Vallejo, CA 94591`,
+  `104 Smith Store Street
+Huntington Beach, CA 92646`,
+  `9321 NW. Hartford Street
+Santa Ana, CA 92704`,
+  `8272 Augusta Street
+Salinas, CA 93905`,
+  `9558 Jockey Hollow Dr.
+Cerritos, CA 90703`,
+  `7 York Rd.
+Santa Ana, CA 92707`,
+  `7255 E. Indian Spring Avenue
+San Diego, CA 92154`,
+  `8314 Griffin Avenue
+Livermore, CA 94550`,
+  `689 Liberty St.
+La Puente, CA 91744`,
+  `8473 Mechanic Street
+Canyon Country, CA 91351`,
+  `922 Saxon Ave.
+Los Angeles, CA 90034`,
+];
